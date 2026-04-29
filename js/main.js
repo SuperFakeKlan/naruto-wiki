38b992873fb217ebf86fb6f6a89bf88b369ca8e6
Diego López Ariza <diegoawesome2017@outlook.com>
2026-04-28 19:05:22 -0600

    Actualizacion de calidad de vida
    - Separación de responsabilidades en funciones
    - Adición de JSDoc para mejor predictibilidad

--- js/main.js
+++ js/main.js
@@ -1,38 +1,90 @@
+/**
+ * Carga los personajes desde un URL
+ * @param {String} url fuente de datos o link
+ * @returns {{data:{
+ * nombre: string,
+ * clan: string,
+ * imagen: string,
+ * descripcion: string,
+ * aldea: string
+ * }[] | undefined,error: string | undefined}} personajes
+ */
+async function loadCharacters(url) {
+    if (!url || typeof url !== "string") return {
+        error: 'URl inválido'
+    }
+    try {
+        const result = await fetch(url)
+        const data = await result.json()
+        return {
+            data
+        }
+    } catch (error) {
+        return {
+            error: 'Error al hacer fetch'
+        }
+    }
+}
+/**
+ * 
+ * @param {Object} character character
+ * @param {string} character.imagen path hacia la imagen
+ * @param {string} character.nombre nombre del personaje
+ * @param {string} character.clan clan del personaje
+ * @param {string} character.descripcion descripcion del personaje
+ * @param {HTMLDivElement} div contenedor donde colocar la card
+ */
+function generateCard({
+    clan, descripcion, imagen, nombre
+}, div) {
+    if (!div) {
+        console.warn(`Ojo: No existe un contenedor con ID "${div}" en tu HTML.`);
+        return
+    }
+    const card = document.createElement("div")
+    const innerCard = document.createElement("div")
+    const frontCard = document.createElement("div")
+    const backCard = document.createElement("div")
+    const image = document.createElement("img")
+    const name = document.createElement("h3")
+    const cardClan = document.createElement("p")
+    const description = document.createElement("p")
+    const button = document.createElement("button")
+    card.classList.add("flip-card")
+    innerCard.classList.add("flip-card-inner")
+    frontCard.classList.add("flip-card-front")
+    backCard.classList.add("flip-card-back")
+    description.classList.add("descripcion")
+    button.classList.add("btn-perfil")
+    image.src = imagen
+    image.alt = nombre
+    name.innerText = nombre
+    cardClan.innerText = clan
+    description.innerText = descripcion
+    button.innerText = "Ver mas..."
+    frontCard.append(image, name, cardClan)
+    backCard.append(cardClan, description)
+    innerCard.append(frontCard, backCard)
+    card.appendChild(innerCard)
+    div.appendChild(card)
+}
+
 async function cargarPersonajes() {
     try {
-        const respuesta = await fetch('./data/personajes.json');
-        const personajes = await respuesta.json();
-
-        personajes.forEach(pj => {
-            // 1. Creamos el molde de tu tarjeta (Flip Card)
-            // Usamos las propiedades exactas de tu JSON
-            const tarjetaHTML = `
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="flip-card-front">
-                            <img src="${pj.imagen}" alt="${pj.nombre}">
-                            <h3>${pj.nombre}</h3>
-                            <p>${pj.clan}</p>
-                        </div>
-                        <div class="flip-card-back">
-                            <h3>${pj.nombre}</h4>
-                            <p class="descripcion">${pj.descripcion}</p>
-                            <button class="btn-perfil">ver mas...</button>
-                    </div>
-                </div>
-            `;
-
-            // 2. Buscamos el contenedor según la aldea (ej: contenedor-konoha)
-            const idContenedor = `contenedor-${pj.aldea.toLowerCase()}`;
-            const contenedor = document.getElementById(idContenedor);
-
-            // 3. Inyectamos la tarjeta si el contenedor existe
-            if (contenedor) {
-                contenedor.innerHTML += tarjetaHTML;
-            } else {
-                console.warn(`Ojo: No existe un contenedor con ID "${idContenedor}" en tu HTML.`);
-            }
-        });
+        const { data: personajes, error } = await loadCharacters('./data/personajes.json')
+        // 1. Buscamos el contenedor según la aldea (ej: contenedor-konoha)
+        console.log(personajes, error)
+        if (!personajes) return
+        // 2. Creamos el molde de tu tarjeta (Flip Card)
+        const konohas = personajes
+            .filter(ch => ch.aldea === "Konoha")
+        const arenas = personajes
+            .filter(ch => ch.aldea === "Arena")
+        const divArena = document.getElementById("contenedor-arena")
+        const divKonoha = document.getElementById("contenedor-konoha")
+        // Usamos las propiedades exactas de tu JSON
+        konohas.forEach((ch) => generateCard(ch, divKonoha))
+        arenas.forEach((ch) => generateCard(ch, divArena))
 
     } catch (error) {
         console.error("Error al renderizar:", error);
